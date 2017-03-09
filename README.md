e0d22e31e597cdeaeea1f8f56bf2e6725e37710c
HeyGary <1762648394@qq.com>
2017-03-09 22:50:46 +0800

    todolist

---
+++ README.md
@@ -0,0 +1,23 @@
+# mynotepad
+
+> 一个Vue.js做的笔记本应用（类似TodoList）
+
+## 技术栈
+vue2 + vuex + webpack + ES6/7 + ElementUI
+
+## 构建过程
+
+
+``` bash
+# 项目运行
+npm install
+
+# 编译环境
+npm run dev
+
+访问 http://localhost:8888
+
+# 线上版本
+npm run build
+
+```
---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <n-header></n-header>
+    <n-add></n-add>
+    <n-list></n-list>
+    <n-sidebar></n-sidebar>
+    <n-dialog></n-dialog>
+    <n-table></n-table>
+  
+  </div>
+</template>
+
+<script>
+    import nHeader from './components/header.vue';
+    import nAdd from './components/event_add.vue';
+    import nTable from './components/event_table.vue';
+    import nList from './components/event_list.vue';
+    import nSidebar from './components/sidebar.vue';
+    import nDialog from './components/dialog.vue';
+export default {
+        data(){
+          
+        },
+        components:{
+            nHeader,
+            nAdd,
+            nSidebar,
+            nDialog,
+            nTable,
+            nList
+        },
+        methods:{
+          
+        }
+}
+</script>
+
+<style>
+
+</style>
